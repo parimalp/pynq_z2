2eafb34e82ba08e180a0eaebef081b4be09c04bf
Parimal Patel <parimalp@xilinx.com>
2017-12-21 16:00:10 -0800

    Updated audio application with correct registers settings

--- audio/src/audio.c
+++ audio/src/audio.c
@@ -28,20 +28,19 @@ void SelectLineIn(void);
 void record(unsigned long u32MemOffset, unsigned long u32NrSamples);
 void play(unsigned long u32MemOffset, unsigned long u32NrSamples);
 void AudioStraight(unsigned long u32NrSamples);
-
 void AudioConfigureJacks(void);
 
 //Global variables
 XIicPs Iic;
 #define DDR_MEMORY 0x10000000
-#define AUD_NR_SAMPLES 1000000
+#define AUD_NR_SAMPLES 200000 // 1000000
 #define DDR_OFFSET_AUD 0
 
 int main(void)
 {
 
 	u32 push, switches;
-	u32 flag;
+	u32 flag; //, LineInSelected, MICInSelected;
 	xil_printf("PYNQ-Z2 Audio Test\r\n");
 
 	//Configure the IIC data structure
@@ -53,7 +52,10 @@ int main(void)
 	//Configure the mixers for Line in, MIC in, and Line out ports.
 	//Call AudioCODECConfig() for configuring Line In, Mic In, and Line Out
 	AudioCODECConfig();
-	xil_printf("ADAU1761 configured\n\r");
+
+	print("ADAU1761 configured\n\r");
+	print("Make sure to turn on SW0 for Line In, SW1 for MIC In\r\n");
+	print("SW0 is sampled first. If both switches are OFF then program ends\r\n");
 	while(1) {
 		switches = Xil_In32(XPAR_SWITCHES_GPIO_BASEADDR);
 		if(switches&01) {
@@ -65,7 +67,7 @@ int main(void)
 			SelectMic();
 		}
 		else {
-			print("Neither Mic nor LineIn selected\r\n");
+			print("Neither MIC nor LineIn selected\r\n");
 			MuteAll();		// DeselectLineIn would do the same
 			print("Exiting...\r\n");
 			exit(0);
@@ -84,17 +86,17 @@ int main(void)
 					print("Done...\r\n");
 					break;
 				}
-				case 0x4:
-				{
-					xil_printf("Playing %d recorded samples\r\n",AUD_NR_SAMPLES);
-					play(DDR_OFFSET_AUD,AUD_NR_SAMPLES);
-					print("Done...\r\n");
-					break;
-				}
 				case 0x2:
 				{
 					xil_printf("Recording %d samples\r\n",AUD_NR_SAMPLES);
 					record(DDR_OFFSET_AUD,AUD_NR_SAMPLES);
+					print("Done...\r\n");
+					break;
+				}
+				case 0x4:
+				{
+					xil_printf("Playing %d recorded samples\r\n",AUD_NR_SAMPLES);
+					play(DDR_OFFSET_AUD,AUD_NR_SAMPLES);
 					print("Done...\r\n");
 					break;
 				}
@@ -111,18 +113,26 @@ int main(void)
 }
 
 void SelectMic(void) {
-	AudioWriteToReg(R5_RECORD_MIXER_LEFT_CONTROL_1, 0x08);
-	AudioWriteToReg(R7_RECORD_MIXER_RIGHT_CONTROL_1, 0x0C);
+	AudioWriteToReg(R4_RECORD_MIXER_LEFT_CONTROL_0, 0x01); // Mixer 1 (left channel)
+	AudioWriteToReg(R5_RECORD_MIXER_LEFT_CONTROL_1, 0x10); // LDBOOST, set to 20 dB
+	AudioWriteToReg(R8_LEFT_DIFFERENTIAL_INPUT_VOLUME_CONTROL, 0xB3);  // LDVOL, set to 21 dB
+
+	AudioWriteToReg(R6_RECORD_MIXER_RIGHT_CONTROL_0, 0x01); // Mixer 2 (right channel)
+	AudioWriteToReg(R7_RECORD_MIXER_RIGHT_CONTROL_1, 0x10); // RDBOOST, set to 20 dB
+	AudioWriteToReg(R9_RIGHT_DIFFERENTIAL_INPUT_VOLUME_CONTROL, 0xB3); // RDVOL, set to 21 dB
 }
 
 void MuteAll(void) {
-	AudioWriteToReg(R5_RECORD_MIXER_LEFT_CONTROL_1, 0x00);  //mute left channel input
-	AudioWriteToReg(R7_RECORD_MIXER_RIGHT_CONTROL_1, 0x00); //mute right channel input
+	AudioWriteToReg(R4_RECORD_MIXER_LEFT_CONTROL_0, 0x00);  //mute Mixer 1 (left channel)
+	AudioWriteToReg(R6_RECORD_MIXER_RIGHT_CONTROL_0, 0x00); //mute Mixer2 (right channel)
 }
 
 void SelectLineIn(void) {
-	AudioWriteToReg(R5_RECORD_MIXER_LEFT_CONTROL_1, 0x05);
-	AudioWriteToReg(R7_RECORD_MIXER_RIGHT_CONTROL_1, 0x05);
+	AudioWriteToReg(R4_RECORD_MIXER_LEFT_CONTROL_0, 0x01); // Mixer 1  (left channel)
+	AudioWriteToReg(R5_RECORD_MIXER_LEFT_CONTROL_1, 0x07); // Enable LAUX (MX1AUXG)
+
+	AudioWriteToReg(R6_RECORD_MIXER_RIGHT_CONTROL_0, 0x01); // Mixer 2
+	AudioWriteToReg(R7_RECORD_MIXER_RIGHT_CONTROL_1, 0x07); // Enable RAUX (MX2AUXG)
 }
 
 /******************************************************************************
@@ -130,9 +140,18 @@ void SelectLineIn(void) {
  * @param	u32NrSamples is the number of samples to read and output.
  * @return	none.
  *****************************************************************************/
-void AudioStraight(unsigned long u32NrSamples)
+void LineInLineOutAudioStraight(unsigned long u32NrSamples)
 {
 	u32  temp, u32Temp;
+	// Use Left/Right input mixers (MIXER1 and MIXER2 output) and set them to 6 dB
+	AudioWriteToReg(R23_PLAYBACK_MIXER_LEFT_CONTROL_1, 0x08);
+	AudioWriteToReg(R25_PLAYBACK_MIXER_RIGHT_CONTROL_1, 0x80);
+	// Enable Mixer3 and Mixer4
+	AudioWriteToReg(R22_PLAYBACK_MIXER_LEFT_CONTROL_0, 0x01);
+	AudioWriteToReg(R24_PLAYBACK_MIXER_RIGHT_CONTROL_0, 0x01);
+	// Enable Left/Right Headphone out
+	AudioWriteToReg(R29_PLAYBACK_HEADPHONE_LEFT_VOLUME_CONTROL, 0xE7);
+	AudioWriteToReg(R30_PLAYBACK_HEADPHONE_RIGHT_VOLUME_CONTROL, 0xE7);
 	for(temp=0; temp < u32NrSamples; temp++)
 	{
 		do //wait for RX data to become available
@@ -141,10 +160,46 @@ void AudioStraight(unsigned long u32NrSamples)
 		} while ( u32Temp == 0);
 		Xil_Out32(I2S_STATUS_REG, 0x00000001); //Clear data rdy bit
 
-		/* Read the sample from and output to the codec */
 		Xil_Out32(I2S_DATA_TX_L_REG, Xil_In32(I2S_DATA_RX_L_REG));	// left
 		Xil_Out32(I2S_DATA_TX_R_REG, Xil_In32(I2S_DATA_RX_R_REG));	// right
 	}
+	AudioWriteToReg(R23_PLAYBACK_MIXER_LEFT_CONTROL_1, 0x00);
+	AudioWriteToReg(R25_PLAYBACK_MIXER_RIGHT_CONTROL_1, 0x00);
+	AudioWriteToReg(R22_PLAYBACK_MIXER_LEFT_CONTROL_0, 0x00);
+	AudioWriteToReg(R24_PLAYBACK_MIXER_RIGHT_CONTROL_0, 0x00);
+	AudioWriteToReg(R29_PLAYBACK_HEADPHONE_LEFT_VOLUME_CONTROL, 0xE5); // Mute LHP
+	AudioWriteToReg(R30_PLAYBACK_HEADPHONE_RIGHT_VOLUME_CONTROL, 0xE5); // Mute RHP
+}
+
+void AudioStraight(unsigned long u32NrSamples)
+{
+	u32  temp, u32Temp;
+	// Use Left/Right input and set them to 6 dB
+	AudioWriteToReg(R23_PLAYBACK_MIXER_LEFT_CONTROL_1, 0x00); // Mixer1 input muted
+	AudioWriteToReg(R25_PLAYBACK_MIXER_RIGHT_CONTROL_1, 0x00); // Mixer2 input muted
+	// Enable Mixer3 and Mixer4
+	AudioWriteToReg(R22_PLAYBACK_MIXER_LEFT_CONTROL_0, 0x21);
+	AudioWriteToReg(R24_PLAYBACK_MIXER_RIGHT_CONTROL_0, 0x41);
+	// Enable Left/Right Headphone out
+	AudioWriteToReg(R29_PLAYBACK_HEADPHONE_LEFT_VOLUME_CONTROL, 0xE7);
+	AudioWriteToReg(R30_PLAYBACK_HEADPHONE_RIGHT_VOLUME_CONTROL, 0xE7);
+	for(temp=0; temp < u32NrSamples; temp++)
+	{
+		do //wait for RX data to become available
+		{
+			u32Temp = Xil_In32(I2S_STATUS_REG);
+		} while ( u32Temp == 0);
+		Xil_Out32(I2S_STATUS_REG, 0x00000001); //Clear data rdy bit
+
+		Xil_Out32(I2S_DATA_TX_L_REG, Xil_In32(I2S_DATA_RX_L_REG));	// left
+		Xil_Out32(I2S_DATA_TX_R_REG, Xil_In32(I2S_DATA_RX_R_REG));	// right
+	}
+	AudioWriteToReg(R23_PLAYBACK_MIXER_LEFT_CONTROL_1, 0x00);
+	AudioWriteToReg(R25_PLAYBACK_MIXER_RIGHT_CONTROL_1, 0x00);
+	AudioWriteToReg(R22_PLAYBACK_MIXER_LEFT_CONTROL_0, 0x00);
+	AudioWriteToReg(R24_PLAYBACK_MIXER_RIGHT_CONTROL_0, 0x00);
+	AudioWriteToReg(R29_PLAYBACK_HEADPHONE_LEFT_VOLUME_CONTROL, 0xE5); // Mute LHP
+	AudioWriteToReg(R30_PLAYBACK_HEADPHONE_RIGHT_VOLUME_CONTROL, 0xE5); // Mute RHP
 }
 
 /******************************************************************************
@@ -185,6 +240,13 @@ void record(unsigned long u32MemOffset, unsigned long u32NrSamples)
 void play(unsigned long u32MemOffset, unsigned long u32NrSamples)
 {
 	u32  temp, u32Temp;
+	// Unmute left and right DAC which gets samples from DAC_SDATA port
+	// Enable Mixer3 and Mixer4
+	AudioWriteToReg(R22_PLAYBACK_MIXER_LEFT_CONTROL_0, 0x21);
+	AudioWriteToReg(R24_PLAYBACK_MIXER_RIGHT_CONTROL_0, 0x41);
+	// Enable Left/Right Headphone out
+	AudioWriteToReg(R29_PLAYBACK_HEADPHONE_LEFT_VOLUME_CONTROL, 0xE7); // Mute LHP
+	AudioWriteToReg(R30_PLAYBACK_HEADPHONE_RIGHT_VOLUME_CONTROL, 0xE7); // Mute RHP
 
 	for(temp=0; temp < (2*u32NrSamples); temp++)
 	{
@@ -199,6 +261,12 @@ void play(unsigned long u32MemOffset, unsigned long u32NrSamples)
 		temp++;
 		Xil_Out32(I2S_DATA_TX_R_REG, Xil_In32(DDR_MEMORY+u32MemOffset+temp*4));	// right
 	}
+	// Mute left and right DAC which gets samples from DAC_SDATA port
+	AudioWriteToReg(R22_PLAYBACK_MIXER_LEFT_CONTROL_0, 0x01);
+	AudioWriteToReg(R24_PLAYBACK_MIXER_RIGHT_CONTROL_0, 0x01);
+	// Mute left input to mixer3 (R23) and right input to mixer4 (R25)
+	AudioWriteToReg(R23_PLAYBACK_MIXER_LEFT_CONTROL_1, 0x00);
+	AudioWriteToReg(R25_PLAYBACK_MIXER_RIGHT_CONTROL_1, 0x00);
 }
 
 
@@ -323,25 +391,49 @@ void AudioWriteToReg(unsigned char u8RegAddr, unsigned char u8Data) {
  * ---------------------------------------------------------------------------- */
 void AudioCODECConfig() {
 	// input path control registers are configured in SelectMic and SelectLineIn
-	AudioWriteToReg(R4_RECORD_MIXER_LEFT_CONTROL_0, 0x01);
-	AudioWriteToReg(R6_RECORD_MIXER_RIGHT_CONTROL_0, 0x01);
-	AudioWriteToReg(R8_LEFT_DIFFERENTIAL_INPUT_VOLUME_CONTROL, 0xB2);
-	AudioWriteToReg(R9_RIGHT_DIFFERENTIAL_INPUT_VOLUME_CONTROL, 0xB2);
+	// Mute Mixer1 and Mixer2 here, enable when MIC and Line In used
+	AudioWriteToReg(R4_RECORD_MIXER_LEFT_CONTROL_0, 0x00);
+	AudioWriteToReg(R6_RECORD_MIXER_RIGHT_CONTROL_0, 0x00);
+	// Set LDVOL and RDVOL to 21 dB and Enable left and right differential enabled
+	AudioWriteToReg(R8_LEFT_DIFFERENTIAL_INPUT_VOLUME_CONTROL, 0xB3);
+	AudioWriteToReg(R9_RIGHT_DIFFERENTIAL_INPUT_VOLUME_CONTROL, 0xB3);
+	// Enable MIC bias
 	AudioWriteToReg(R10_RECORD_MICROPHONE_BIAS_CONTROL, 0x01);
+	// Enable ALC control and noise gate
 	AudioWriteToReg(R14_ALC_CONTROL_3, 0x20);
+	// Put CODEC in Master mode
 	AudioWriteToReg(R15_SERIAL_PORT_CONTROL_0, 0x01);
+	// Enable ADC on both channels, normal polarity and ADC high-pass filter
 	AudioWriteToReg(R19_ADC_CONTROL, 0x33);
-	AudioWriteToReg(R22_PLAYBACK_MIXER_LEFT_CONTROL_0, 0x21);
-	AudioWriteToReg(R24_PLAYBACK_MIXER_RIGHT_CONTROL_0, 0x41);
-	AudioWriteToReg(R28_PLAYBACK_LR_MIXER_MONO_OUTPUT_CONTROL, 0x01);
-	AudioWriteToReg(R29_PLAYBACK_HEADPHONE_LEFT_VOLUME_CONTROL, 0xE7);
-	AudioWriteToReg(R33_PLAYBACK_MONO_OUTPUT_CONTROL, 0xE5);
+	// Mute play back Mixer3 and Mixer4 and enable when output is required (straight and play back modes)
+	AudioWriteToReg(R22_PLAYBACK_MIXER_LEFT_CONTROL_0, 0x00); // 0x21);
+	AudioWriteToReg(R24_PLAYBACK_MIXER_RIGHT_CONTROL_0, 0x00); // 0x41);
+	// Mute left input to mixer3 (R23) and right input to mixer4 (R25)
+	// Enable them in straight play mode
+	AudioWriteToReg(R23_PLAYBACK_MIXER_LEFT_CONTROL_1, 0x00);
+	AudioWriteToReg(R25_PLAYBACK_MIXER_RIGHT_CONTROL_1, 0x00);
+	// MONO output control at Mixer7 not required
+	// AudioWriteToReg(R28_PLAYBACK_LR_MIXER_MONO_OUTPUT_CONTROL, 0x01);
+	// Mute left and right channels output; enable them when output is needed (straight and play back modes)
+	AudioWriteToReg(R29_PLAYBACK_HEADPHONE_LEFT_VOLUME_CONTROL, 0xE5);
+	AudioWriteToReg(R30_PLAYBACK_HEADPHONE_RIGHT_VOLUME_CONTROL, 0xE5);
+	// MONO output control not required
+	// AudioWriteToReg(R33_PLAYBACK_MONO_OUTPUT_CONTROL, 0xE5);
+	// Enable play back right and left channels
 	AudioWriteToReg(R35_PLAYBACK_POWER_MANAGEMENT, 0x03);
+	// Enable DAC for both channels
 	AudioWriteToReg(R36_DAC_CONTROL_0, 0x03);
+	// Set SDATA_In to DAC
 	AudioWriteToReg(R58_SERIAL_INPUT_ROUTE_CONTROL, 0x01);
+	// Set SDATA_Out to ADC
 	AudioWriteToReg(R59_SERIAL_OUTPUT_ROUTE_CONTROL, 0x01);
+	// Enable DSP and DSP Run
 	AudioWriteToReg(R61_DSP_ENABLE, 0x01);
 	AudioWriteToReg(R62_DSP_RUN, 0x01);
+	// Enables the digital clock engine for different blocks within the ADAU1761
+	// All clocks are enabled
 	AudioWriteToReg(R65_CLOCK_ENABLE_0, 0x7F);
+	// Enable Digital Clock Generator 0 and 1. Generator 0 generates sample rates for the ADCs,
+	// DACs, and DSP. Generator 1 generates BCLK and LRCLK for the serial port.
 	AudioWriteToReg(R66_CLOCK_ENABLE_1, 0x03);
 }
